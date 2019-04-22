c68a59e10335a6f975daf4f45d6ae8ea7ddabe50
Mark Bennett <mark.bennett@ucf.edu>
2019-04-22 14:53:00 -0400

    sidebar

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );// add parent style
 
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-   wp_enqueue_style( 'dtl-style', get_stylesheet_directory_uri() .'/assets/css/dtl-college-theme.css');
+   wp_enqueue_style( 'dtl-style', get_stylesheet_directory_uri() .'/assets/css/dtltheme.css');
 }
 
 define( 'DTL_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
