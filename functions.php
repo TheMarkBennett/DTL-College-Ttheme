2471eab75f671a147d69b95d844cfb7eba781f6f
Mark Bennett <mark.bennett@ucf.edu>
2019-04-22 12:55:50 -0400

    init

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );// add parent style
+
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+   wp_enqueue_style( 'post-style', get_stylesheet_directory_uri() .'/assets/css/dtl-college-theme.css', array(),'1.0.0' );
+}
+
+define( 'DTL_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
+
+
+//custom image sizes
+require_once( PROVOST_NEWS_THEME_DIR . 'includes/theme-options.php'); //custom image sizes
